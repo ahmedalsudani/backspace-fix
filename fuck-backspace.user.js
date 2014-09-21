9cd21630b761d4bd64ca55150a328dad6030330d
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2014-09-21 16:57:56 -0400

    Genesis

---
+++ fuck-backspace.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name            Fuck backspace
+// @namespace       ahmed.al-sudani.com/projects/backspace-back-fix
+// @version         0.1.1
+// @description     Script that prevents the default backspace behavior. Seems to work just fine everywhere.
+//                  License: MIT License
+// @match           http*://*/*
+// @run-at          document-start
+// @copyright       2014, Ahmed Al-Sudani
+// ==/UserScript==
+
+// As per http://stackoverflow.com/a/25099617/1009017
+document.addEventListener('keydown', stopDefaultBackspaceBehaviour);
+document.addEventListener('keypress', stopDefaultBackspaceBehaviour);
+
+
+function stopDefaultBackspaceBehaviour(event) {
+    'use strict';
+    event = event || window.event;
+    if (event.keyCode === 8) {
+        var elements = "HTML, BODY, TABLE, TBODY, TR, TD, DIV";
+        var d = event.srcElement || event.target;
+        var regex = new RegExp(d.tagName.toUpperCase());
+        if (! d.contentEditable) {
+            if (regex.test(elements)) {
+                event.preventDefault ? event.preventDefault() : event.returnValue = false;
+            }
+        }
+    }
+}
